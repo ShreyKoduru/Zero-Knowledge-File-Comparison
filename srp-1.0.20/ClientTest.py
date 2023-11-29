5e42929440ec7e85909b320245892e471054d112
keita <keitapoarch@gmail.com>
2023-11-28 22:19:42 -0500

    updated client and server

--- srp-1.0.20/ClientTest.py
+++ srp-1.0.20/ClientTest.py
@@ -2,37 +2,45 @@ import socket
 import srp
 
 def run_client():
+
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('localhost', 12345))
+    client.connect(('192.168.137.129', 5005))
+    
+    for i in range(5):
+        with open('C:/Users/keita/Desktop/client files/test1.py',"r") as f:
+            string = f.read()
+        print(string)
+        
+        # SRP Client Logic
+        username = 'user'
+        password = string
 
-    # SRP Client Logic
-    username = 'user'
-    password = 'password'
+        usr = srp.User(username, str(password))
+        uname, A = usr.start_authentication()
 
-    usr = srp.User(username, password)
-    uname, A = usr.start_authentication()
+        # Send username and A to server
+        client.send(username.encode())
+        client.send(A)
 
-    # Send username and A to server
-    client.send(username.encode())
-    client.send(A)
+        # Receive salt and B from server
+        salt = client.recv(1024)
+        B = client.recv(1024)
 
-    # Receive salt and B from server
-    salt = client.recv(1024)
-    B = client.recv(1024)
+        # Process challenge
+        M = usr.process_challenge(salt, B)
+        client.send(M)
 
-    # Process challenge
-    M = usr.process_challenge(salt, B)
-    client.send(M)
+        # Receive HAMK from server and verify session
+        HAMK = client.recv(1024)
+        usr.verify_session(HAMK)
 
-    # Receive HAMK from server and verify session
-    HAMK = client.recv(1024)
-    usr.verify_session(HAMK)
-
-    if usr.authenticated():
-        print("Authentication Successful")
-    else:
-        print("Authentication Failed")
-
+        if usr.authenticated():
+            print("FILES ARE THE SAME")
+        else:
+            print("FILES ARE NOT THE SAME")
+            
+            
+        
     client.close()
 
 run_client()
--- srp-1.0.20/ServerTest.py
+++ srp-1.0.20/ServerTest.py
@@ -9,7 +9,10 @@ def handle_client(client_socket):
 
         # Generate salt and verifier for the given username
         # This should be stored from the time of user registration
-        salt, vkey = srp.create_salted_verification_key(username, 'password1')
+        with open('/Users/shreykoduru/Desktop/server files/test copy1.py',"r") as f:
+            string = f.read()
+        print(string)
+        salt, vkey = srp.create_salted_verification_key(username, str(string))
 
         svr = srp.Verifier(username, salt, vkey, A)
         s, B = svr.get_challenge()
@@ -21,34 +24,39 @@ def handle_client(client_socket):
         # Receive M from client and verify session
         M = client_socket.recv(1024)
         HAMK = svr.verify_session(M)
-        
+
         if HAMK is None:
             print("authentification Failed")
+            client_socket.close()
             exit(0)
 
         # Send HAMK to client
-        client_socket.send(HAMK.encode())
+        client_socket.send(HAMK)
 
         if svr.authenticated():
-            print("Authentication Successful")
+            print("Files are the Same")
+            client_socket.close()
             exit(0)
-            
+
         else:
-            print("Authentication Failed")
+            print("Files Not the Same")
+            client_socket.close()
             exit(0)
-            
+
     finally:
         client_socket.close()
 
 def run_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('localhost', 12345))
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('192.168.137.129', 5005))
     server.listen(5)
     print("Server listening on port 12345")
 
     try:
         while True:
-            client_socket, _ = server.accept()
+            client_socket,  = server.accept()
+            print("accept")
             handle_client(client_socket)
     finally:
         server.close()
