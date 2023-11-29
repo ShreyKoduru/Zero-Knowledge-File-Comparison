c74b97006fee2f6159b3cf95eeab6c62db28fe7d
keita <keitapoarch@gmail.com>
2023-11-28 19:48:17 -0500

    server client works FUCK YEAH

--- srp-1.0.20/ClientTest.py
+++ srp-1.0.20/ClientTest.py
@@ -14,18 +14,18 @@ def run_client():
 
     # Send username and A to server
     client.send(username.encode())
-    client.send(A.encode())
+    client.send(A)
 
     # Receive salt and B from server
-    salt = client.recv(1024).decode()
-    B = client.recv(1024).decode()
+    salt = client.recv(1024)
+    B = client.recv(1024)
 
     # Process challenge
     M = usr.process_challenge(salt, B)
-    client.send(M.encode())
+    client.send(M)
 
     # Receive HAMK from server and verify session
-    HAMK = client.recv(1024).decode()
+    HAMK = client.recv(1024)
     usr.verify_session(HAMK)
 
     if usr.authenticated():
--- srp-1.0.20/ServerTest.py
+++ srp-1.0.20/ServerTest.py
@@ -4,31 +4,39 @@ import srp
 def handle_client(client_socket):
     try:
         # Receive client's username and A value
-        username = client_socket.recv(1024).decode()
-        A = client_socket.recv(1024).decode()
+        username = client_socket.recv(1024)
+        A = client_socket.recv(1024)
 
         # Generate salt and verifier for the given username
         # This should be stored from the time of user registration
-        salt, vkey = srp.create_salted_verification_key(username, 'password')
+        salt, vkey = srp.create_salted_verification_key(username, 'password1')
 
         svr = srp.Verifier(username, salt, vkey, A)
         s, B = svr.get_challenge()
 
         # Send salt and B to client
-        client_socket.send(salt.encode())
-        client_socket.send(B.encode())
+        client_socket.send(salt)
+        client_socket.send(B)
 
         # Receive M from client and verify session
-        M = client_socket.recv(1024).decode()
+        M = client_socket.recv(1024)
         HAMK = svr.verify_session(M)
+        
+        if HAMK is None:
+            print("authentification Failed")
+            exit(0)
 
         # Send HAMK to client
         client_socket.send(HAMK.encode())
 
         if svr.authenticated():
             print("Authentication Successful")
+            exit(0)
+            
         else:
             print("Authentication Failed")
+            exit(0)
+            
     finally:
         client_socket.close()
 
