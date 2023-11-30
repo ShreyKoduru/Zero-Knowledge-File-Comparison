4ee1648c1e4940f2e54d420ea8b9af84a545b2d0
ShreyKoduru <78169157+ShreyKoduru@users.noreply.github.com>
2023-11-29 19:01:46 -0500

    Update ServerTest.py
    Final Commit

--- srp-1.0.20/ServerTest.py
+++ srp-1.0.20/ServerTest.py
@@ -4,38 +4,35 @@ import srp
 def handle_client(client_socket):
     try:
         for j in range(5):
-            for i in range(1, 6):  # Assuming 5 files to compare
+            for i in range(1, 6): 
                 filepath = 'mockserverfiles/test copy' + str(i) + '.py'
                 with open(filepath, "r") as f:
                     file_content = f.read()
-            # Receive client's username and A value
+            
                 username = client_socket.recv(1024)
                 A = client_socket.recv(1024)
 
-                # Generate salt and verifier for the given username
-                # This should be stored from the time of user registration
+                
                 salt, vkey = srp.create_salted_verification_key(username, str(file_content))
 
                 svr = srp.Verifier(username, salt, vkey, A)
                 s, B = svr.get_challenge()
 
-                # Send salt and B to client
+                
                 client_socket.send(salt)
                 client_socket.send(B)
 
-                # Receive M from client and verify session
+    
                 M = client_socket.recv(1024)
                 HAMK = svr.verify_session(M)
                 
-                #HAMK = b'r2\xa7\xd8\xfaXy\xbd\xfc\x0cN\xa4\x9b\xfaf\xb62\xef\x02\xcc'
-                
+        
                 if HAMK is None:
                     print("Files Not the Same ")
                     HAMK = b'00000000000000000000000'
                     client_socket.send(HAMK)
                     continue
-
-                # Send HAMK to client
+                
                 client_socket.send(HAMK)
 
                 if svr.authenticated():
@@ -52,7 +49,7 @@ def handle_client(client_socket):
 def run_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(('192.168.137.199', 5005))
+    server.bind(('192.168.137.129', 5005))
     server.listen(5)
     print("Server listening on port 12345")
 
